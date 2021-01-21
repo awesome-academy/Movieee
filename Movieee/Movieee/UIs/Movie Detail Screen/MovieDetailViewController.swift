f0b13626cc118030249faaf60af8f32178cc0472
minhtong210 <minhtong210@gmail.com>
2021-01-21 13:27:41 +0700

    Movie Detail Screen

---
+++ Movieee/Movieee/UIs/Movie Detail Screen/MovieDetailViewController.swift
@@ -0,0 +1,138 @@
+import UIKit
+private enum ConstraintsMovieDetail {
+    static let idGenreCell = "GenreCollectionCell"
+    static let idCastCell = "CastCollectionCell"
+    static let sizeOfItem = CGSize(width: 100, height: 200)
+}
+
+final class MovieDetailViewController: UIViewController {
+    @IBOutlet private weak var posterImage: UIImageView!
+    @IBOutlet private weak var filmNameLabel: UILabel!
+    @IBOutlet private weak var filmDuration: UILabel!
+    @IBOutlet private weak var filmReleaseDay: UILabel!
+    @IBOutlet private weak var filmDescription: UILabel!
+    @IBOutlet private weak var voteRate: UILabel!
+    @IBOutlet private weak var genreCollection: UICollectionView!
+    @IBOutlet private weak var castCollection: UICollectionView!
+    @IBOutlet private var ratingStars: [UIImageView]!
+    var idFilm = 0
+    
+    private var movieDetail: MovieDetail? {
+        didSet {
+            genreCollection.reloadData()
+        }
+    }
+    private var castMovieDetail = [CastInMovieDetail]() {
+        didSet {
+            castCollection.reloadData()
+        }
+    }
+    
+    private let dispatchGroup = DispatchGroup()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        genreCollection.delegate = self
+        castCollection.delegate = self
+        genreCollection.dataSource = self
+        castCollection.dataSource = self
+        configMovieDetail(with: idFilm)
+    }
+    
+    private func configMovieDetail(with id: Int) {
+        dispatchGroup.enter()
+        APIMovie.apiMovie.getMovieDetail(movieId: String(id)) { [unowned self] result in
+            if let result = result {
+                movieDetail = result
+                configInfo(movieInfo: result)
+            }
+        }
+        APIMovie.apiMovie.getMovieCredits(movieId: String(id)) { [unowned self] result in
+            if let result = result {
+                castMovieDetail = result.cast
+            }
+        }
+        dispatchGroup.leave()
+    }
+    
+    private func configInfo(movieInfo: MovieDetail) {
+        posterImage.getImageFromURL(imgURL: UrlAPIMovie.urlMovieImage + movieInfo.poster)
+        filmNameLabel.text = movieInfo.title
+        filmDuration.text = "\(movieInfo.runtime / 60)h \(movieInfo.runtime % 60)m"
+        filmDescription.text = movieInfo.overview
+        filmReleaseDay.text = movieInfo.releaseDay
+        voteRate.text = "\(movieInfo.voteAverage)"
+        configStarBar(rate: movieInfo.voteAverage)
+    }
+    
+    private func configStarBar(rate: Double) {
+        voteRate.text = "\(rate)"
+        let numStar = Int(rate / 2)
+        for index in 0..<ratingStars.count {
+            ratingStars[index].tintColor = index < numStar ? .systemYellow : .lightGray
+        }
+    }
+    
+    @IBAction func viewTrailerPressed(_ sender: UIButton) {
+        //Navigation to Trailer Detail Screen
+        //Use idFilm to pass
+    }
+    
+    @IBAction func likeButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension MovieDetailViewController: UICollectionViewDelegate,
+                                     UICollectionViewDataSource,
+                                     UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        if collectionView == genreCollection {
+            return movieDetail?.genres.count ?? 0
+        } else {
+            return castMovieDetail.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == genreCollection {
+            guard let genreCell = collectionView.dequeueReusableCell(withReuseIdentifier: ConstraintsMovieDetail.idGenreCell,
+                                                                     for: indexPath) as? MovieDetailGenreCollectionCell,
+                  let genre = movieDetail?.genres[indexPath.row]
+            else { return UICollectionViewCell() }
+            
+            genreCell.configMovieGenreCell(movieDetailGenre: genre)
+            return genreCell
+        } else {
+            guard let castCell = collectionView.dequeueReusableCell(withReuseIdentifier: ConstraintsMovieDetail.idCastCell ,
+                                                                    for: indexPath) as? CastCollectionCell
+            else { return UICollectionViewCell() }
+            
+            castCell.configCastCell(castList: castMovieDetail[indexPath.row])
+            return castCell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return ConstraintsMovieDetail.sizeOfItem
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == genreCollection {
+            let idGenre = movieDetail?.genres[indexPath.row].id
+            //navigation to Genre Detail Screen
+            //Use idGenre to pass
+        } else {
+            let idCast = castMovieDetail[indexPath.row].id
+            //navigation to Person Detail Screen
+            //Use idCast to pass
+        }
+    }
+    
+}
